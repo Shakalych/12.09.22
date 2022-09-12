a2ac6d622b05f9dd9d2db04c67e6a0e10f02f014
Shakalych <madstabr@gmail.com>
2022-09-12 19:31:25 +0500

    1 Задача

---
+++ Exam1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите размер массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+FillArray(M);
+Console.WriteLine($"Введено чисел больше 0: {CountNumbersMoreZero(array)} ");
+
+void FillArray(int M)
+{   
+    
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine("Введите {0} элемнт массива",i);
+        array[i] = Convert.ToInt32(Console.ReadLine()); 
+    }
+    string arr = string.Join(", ", array);
+    Console.WriteLine("Ваш массив: " + $"[{arr}]");
+}
+
+int CountNumbersMoreZero(int[] array)
+{
+    int count = 0;
+    for(int j = 0; j < array.Length; j++)
+    {
+        if(array[j] > 0)
+        count++;    
+    }
+    return count;
+}
